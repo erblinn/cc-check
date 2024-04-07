655aeda83519f88ac7435b1b5709286e8fcd20be
erblinn <45861658+erblinn@users.noreply.github.com>
2024-04-07 20:11:02 +0200

    Update ap.js

--- ap.js
+++ ap.js
@@ -3,19 +3,16 @@ function fillCardNumber() {
     var cardNumberInput = document.getElementById("cardnumber");
     if (cardNumberInput) {
         // Set the value attribute
-        cardNumberInput.setAttribute('value', '4111111111111111');
-        // Dispatch input event
+        cardNumberInput.value = '4111111111111111';
+        
+        // Dispatch input and change events
         cardNumberInput.dispatchEvent(new Event('input', { bubbles: true }));
-        // Dispatch change event
         cardNumberInput.dispatchEvent(new Event('change', { bubbles: true }));
         
-        // Check if the value was successfully set
-        if (cardNumberInput.value === '4111111111111111') {
-            console.log("Card number successfully filled.");
-        } else {
-            console.error("Failed to fill card number.");
-        }
+        // Log success message
+        console.log("Card number successfully filled.");
     } else {
+        // Log error message
         console.error("Input field with ID 'cardnumber' not found.");
     }
 }
